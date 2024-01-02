277d974fface300e31f52914e49cc977ea139f93
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-01-02 21:48:23 +0530

    Array_pair_sum_divisibilty_problem

--- January2024/Array_pair_sum_divisibilty_problem.cpp
+++ January2024/Array_pair_sum_divisibilty_problem.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        // Code here.
+        int n = nums.size();
+        if(n % 2 != 0 ) return false;
+        
+        vector<int> count(k+1, 0);
+        for(auto var : nums){
+            var = var % k;
+            count[var]++;
+            count[k-var]--;
+        }
+        
+        for(int i = 1; i < k; i++) {
+            if(count[i] > 0) return false;
+        }
+        
+        return true;
+    }
+};
