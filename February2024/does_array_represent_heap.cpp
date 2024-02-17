48f44ccc8e5077131cac3b746f67f6c831066c5c
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-02-17 21:29:45 +0530

    does_array_represent_heap

---
+++ February2024/does_array_represent_heap.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    bool isMaxHeap(int arr[], int n)
+    {
+        // Your code goes here
+        for(int i = 0; i < n; i++){
+            if(arr[i] < arr[2*i + 1] || arr[i] < arr[2*i+2]) return false; 
+        }
+        return true;
+    }
+};
