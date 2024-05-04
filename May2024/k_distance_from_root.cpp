5a0f714d837ebc681b564d19c36112c16bda3aa6
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-05-04 21:20:25 +0530

    Update k_distance_from_root

--- May2024/k_distance_from_root.cpp
+++ May2024/k_distance_from_root.cpp
@@ -1,23 +1,22 @@
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-}; */
-
 class Solution
 {
     public:
     // function should print the nodes at k distance from root
+    void solve(struct Node *root,int h,int k,vector<int>&ans){
+        if(!root){
+            return;
+        }
+        if(k == h){
+            ans.push_back(root->data);
+        }
+        solve(root->left,h+1,k,ans);
+        solve(root->right,h+1,k,ans);
+    }
     vector<int> Kdistance(struct Node *root, int k)
     {
-        // Your code here
-         vector<int> v;
-         if(k==0) v.push_back(root->data);
-          else{
-              if(root->left)Kdistance(root->left,k-1);
-              if(root->right)Kdistance(root->right,k-1);
-          }
-          return v;
+      // Your code here
+      vector<int> ans;
+      solve(root,0,k,ans);
+      return ans;
     }
 };
