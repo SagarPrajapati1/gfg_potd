5791e9d03b8f294aec921ede35739b96fcdf9d04
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-06-10 19:34:22 +0530

    nuts_and_bolts_problem

---
+++ June2024/nuts_and_bolts_problem.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+        
+        sort(nuts, nuts+n);
+        sort(bolts, bolts+n);
+    }
+};
