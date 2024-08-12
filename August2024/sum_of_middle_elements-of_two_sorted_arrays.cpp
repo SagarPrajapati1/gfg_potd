afcb1b3f403eb4f27dfbf24709ef044044f73a24
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-08-12 22:27:02 +0530

    sum_of_middle_elements-of_two_sorted_arrays

---
+++ August2024/sum_of_middle_elements-of_two_sorted_arrays.cpp
@@ -0,0 +1,48 @@
+//Position this line where user code will be pasted.
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+        // code here
+        for(auto it : arr2){
+            arr1.push_back(it);
+        }
+        
+        sort(arr1.begin(), arr1.end());
+        int sum = arr1[arr1.size()/2] + arr1[arr1.size()/2 - 1];
+        
+        return sum;
+    }
+};
+
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+        int n = arr1.size();
+        int low = 0, high = n;
+
+        while (low <= high) {
+            // Partition points in both arrays
+            int cut1 = (low + high) / 2;
+            int cut2 = n - cut1;
+
+            // Elements around the partition points
+            int l1 = (cut1 == 0) ? INT_MIN : arr1[cut1 - 1];
+            int l2 = (cut2 == 0) ? INT_MIN : arr2[cut2 - 1];
+            int r1 = (cut1 == n) ? INT_MAX : arr1[cut1];
+            int r2 = (cut2 == n) ? INT_MAX : arr2[cut2];
+
+            // Check if we have found the correct partition
+            if (l1 <= r2 && l2 <= r1) {
+                // Return the sum of the middle elements
+                return max(l1, l2) + min(r1, r2);
+            } else if (l1 > r2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+
+        // This case will never occur for valid input
+        return 0;
+    }
+};
