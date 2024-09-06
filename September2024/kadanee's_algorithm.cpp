ecff2cda7d56a9507426ff86eda6a95598a0c9f2
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-09-06 20:08:27 +0530

    kadanee's_algorithm

---
+++ September2024/kadanee's_algorithm.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        // code here...
+        long long sum = 0;
+        int maxi = INT_MIN;
+        
+        int start = 0;
+        int n = arr.size();
+        
+        for(int i = 0; i < n; i++) {
+            // if (sum == 0) start = i;
+            
+            sum = sum + arr[i];
+            
+            if(sum > maxi ){
+                maxi = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+};
