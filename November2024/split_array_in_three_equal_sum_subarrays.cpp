2ba1dde013c76d44ddb3caf598142cce42b6f4d1
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-11-07 17:34:01 +0530

    split_array_in_three_equal_sum_subarrays

---
+++ November2024/split_array_in_three_equal_sum_subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    // Function to determine if array arr can be split into three equal sum sets.
+    vector<int> findSplit(vector<int>& arr) {
+              vector<int> ans={-1,-1};
+        int sum =accumulate(arr.begin(),arr.end(),0);
+        if(sum%3!=0)return ans; //not possible solution
+        
+        int tar =sum/3;
+        int curr_sum =0;
+        int index =0;
+        for(int i=0;i
+        <arr.size();i++){
+            curr_sum+=arr[i];
+            if(curr_sum ==tar){
+                curr_sum =0;
+                if(index <2)
+                    ans[index++]=i;
+            }
+        }
+        return ans;
+    }
+};
