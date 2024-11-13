08ab2682dbd2f7ded374ca23944bc5e1ff809136
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-11-13 21:41:58 +0530

    intersection_point_in_y_shaped_linked_list

---
+++ November2024/intersection_point_in_y_shaped_linked_list.cpp
@@ -0,0 +1,22 @@
+/* Linked List Node
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution {
+  public:
+    // Function to find intersection point in Y shaped Linked Lists.
+    int intersectPoint(Node* head1, Node* head2) {
+        Node nodeA = head1, nodeB = head2;
+        while(nodeA != nodeB) {
+            nodeA = nodeA.next==null? head2 : nodeA.next;
+            nodeB = nodeB.next == null ? head1 : nodeB.next;
+        }
+        return nodeA.data;
+    }
+};
