3e030a77574222ae9b8347665913947e973a7ab2
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-11-13 21:51:11 +0530

    intersection_in_y_shaped_linked_list

--- November2024/intersection_in_y_shaped_linked_list.cpp
+++ November2024/intersection_in_y_shaped_linked_list.cpp
@@ -12,11 +12,11 @@ class Solution {
   public:
     // Function to find intersection point in Y shaped Linked Lists.
     int intersectPoint(Node* head1, Node* head2) {
-        Node nodeA = head1, nodeB = head2;
+        Node* nodeA = head1; Node* nodeB = head2;
         while(nodeA != nodeB) {
-            nodeA = nodeA.next==null? head2 : nodeA.next;
-            nodeB = nodeB.next == null ? head1 : nodeB.next;
+            nodeA = nodeA->next==nullptr? head2 : nodeA->next;
+            nodeB = nodeB->next == nullptr ? head1 : nodeB->next;
         }
-        return nodeA.data;
+        return nodeA->data;
     }
 };
