b283035ceb0d18241589870c1ec04cefb07950f2
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-11-17 20:53:01 +0530

    reverse_an_array

---
+++ November2024/reverse_an_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int s = 0;
+        int e = arr.size() - 1;
+        while(s <= e){
+            swap(arr[s++], arr[e--]);
+        }
+    }
+};
