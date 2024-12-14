c33b89b7ea0c98c8be28da359c696d6c31301de8
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-12-14 11:24:43 +0530

    search_in_rotated_sorted_array

---
+++ December2024/search_in_rotated_sorted_array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        // complete the function here
+        int s = 0;
+        int e = arr.size() - 1;
+        
+        while(s <= e) {
+            int mid = (e+s)/2;
+            if(arr[mid]  ==  key) return mid;
+            if(arr[s] <= arr[mid]) {
+                if(arr[s] <= key && key < arr[mid]) e = mid - 1;
+                
+                else s = mid + 1;
+            }
+            
+            else {
+                if(arr[mid] < key && key <= arr[e]) s = mid + 1;
+                else e = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
