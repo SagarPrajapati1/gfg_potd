9433fb3e1646c824663c4af11faa527dc804d29d
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2024-12-15 15:53:31 +0530

    peak_element

---
+++ December2024/peak_element.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int peakElement(vector<int> &arr) {
+        // Your code here
+        int n = arr.size();
+        if(n == 1) return 0;        
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n-1] > arr[n-2]) return n-1;
+        
+        int s = 1;
+        int e = n-2;
+        
+        
+        while(s <= e){
+            int mid = (s+e)/2;
+            
+            if(arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid + 1]) return mid;
+            
+            else if(arr[mid] >= arr[mid - 1]) s = mid + 1; // increasing slope
+            else e = mid; // decreasing slope
+            
+            // for multiple peaks
+            // else s = mid + 1
+        }
+        return -1;
+    }
+};
